b959c9d2b477b3bacbd883934954685bb4365ccb
Diego <diegoyon@outlook.com>
2023-04-12 10:35:47 -0600

    fix url

--- lib/webhoo_registration.rb
+++ lib/webhoo_registration.rb
@@ -3,7 +3,7 @@ require 'httparty'
 
 response = HTTParty.post('https://flexhire.com/api/v1/webhooks',
                          headers: { 'Authorization' => 'Bearer nzfdgzr6e3yz28nd' },
-                         body: { url: 'https://your-app-url.com/webhooks/flexhire' })
+                         body: { url: 'https://flexhire.herokuapp.com/webhooks/flexhire' })
 
 if response.success?
   puts 'Webhook registered successfully!'
